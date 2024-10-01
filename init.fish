fd001132b3fd21c18db32704620db58cb1c2a55a
eliss <eliss@openSUSE.>
2024-10-01 16:21:59 +0200

    Added more "init" files

---
+++ init.fish
@@ -0,0 +1,6 @@
+# /usr/bin/fish
+
+echo "function qv
+    ~/qv/build/qv.exe
+end
+" >> ~/.config/fish/config.fish
---
+++ init.ps1
@@ -0,0 +1,3 @@
+function qv {
+    ~/qv/build/qv.exe
+} >> $env:USERPROFILE/Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1

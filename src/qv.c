607394157e75ff8890129ae417cdd8cabfd5c5c0
eliss <staaf.elis@gmail.com>
2024-10-20 01:00:13 +0200

    added more C keywords in syntax highlighting

--- src/qv.c
+++ src/qv.c
@@ -103,8 +103,9 @@ struct editorConfig E;
 char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL };
 char *C_HL_keywords[] = {
   "switch", "if", "while", "for", "break", "continue", "return", "else",
-  "struct", "union", "typedef", "static", "enum", "class", "case",
-  "#define", "#include",
+  "struct", "union", "typedef", "static", "enum", "case", "#define", 
+  "#include", "#if", "#else", "#ifdef", "#ifndef", "#error", "#warning",
+  "#endif", "goto",
 
   "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
   "void|", "true|", "false|", "NULL|"

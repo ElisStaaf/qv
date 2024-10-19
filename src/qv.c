66c930cffb320f39142ba40b798091a0ff6ac95a
eliss <staaf.elis@gmail.com>
2024-10-20 00:02:25 +0200

    Added a comma. I think i'm going insane.

--- src/qv.c
+++ src/qv.c
@@ -104,7 +104,7 @@ char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL };
 char *C_HL_keywords[] = {
   "switch", "if", "while", "for", "break", "continue", "return", "else",
   "struct", "union", "typedef", "static", "enum", "class", "case",
-  "#define", "#include"
+  "#define", "#include",
 
   "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
   "void|", "true|", "false|", "NULL|"

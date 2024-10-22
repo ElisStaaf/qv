8e472dfec5c6c78161488856afd952197798669d
eliss <staaf.elis@gmail.com>
2024-10-22 21:08:01 +0200

    Well... Segfaults can suck.

--- src/qv.c
+++ src/qv.c
@@ -351,19 +351,6 @@ char *R_HL_keywords[] = {
     "function|", "TRUE|", "FALSE|", "NULL|", "Inf|", "NaN|", "NA|",
 };
 
-/* I messed up. I got a segmentation fault when running these extensions and that's because
- * it checks for a dot... I need to repurpose this somehow, but not today.
- */
-char *MAKE_HL_extensions[] = { ".Makefile", ".makefile", ".mk", ".make", ".mak", "GNUmakefile", NULL };
-char *MAKE_HL_keywords[] = {
-    "shell", "include", "ifeq", "else", "endif", "wildcard", "echo", "strip", "filter",
-    "filter-out", "export", "override", "define", "endef", "ifdef", "ifndef", "ifneq",
-    "subst", "patsubst", "foreach", "if", "call", "vpath", "or", "addprefix",
-
-    "true|", "false|", "SHELL|", "MAKE|", ".EXPORT_ALL_VARIABLES|", ".PHONY|",
-    ".DELETE_ON_ERROR|"
-};
-
 /* HLDB stands for HighLighting DataBase, and contains 
  * the settings and initialization for the syntax highlighting */
 struct editorSyntax HLDB[] = {
@@ -561,13 +548,6 @@ struct editorSyntax HLDB[] = {
     HS_HL_extensions,
     HS_HL_keywords, 
     "#", "\"", "\"",
-    HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS,
-  },
-  {
-    "Makefile",
-    MAKE_HL_extensions,
-    MAKE_HL_keywords, 
-    "#", "echo \"", "\"",
     HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS,
   },
 };

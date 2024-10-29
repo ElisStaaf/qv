44ad09a7e761d06983a9e8083d8b4380908eac0e
eliss <staaf.elis@gmail.com>
2024-10-29 22:16:23 +0100

    [#fix]: improve source code comments.

--- src/vine.c
+++ src/vine.c
@@ -835,6 +835,12 @@ void editorFind() {
     }
 }
 
+/* ==================== Terminal Update =================== */
+
+/* We define a very simple "append buffer" structure, that is an heap
+ * allocated string where we can append to. This is useful in order to
+ * write all the escape sequences in a buffer and flush them to the standard
+ * output in a single call, to avoid flickering effects. */
 struct abuf {
     char *b;
     int len;

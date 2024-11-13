4e783566fb8c3fa47576bf606660ddb6119bdc59
eliss <staaf.elis@gmail.com>
2024-11-13 22:41:47 +0100

    STUPID FUCKING FORMATTING AND I'M FUCKING GOING INSANE

--- src/vine.c
+++ src/vine.c
@@ -278,12 +278,12 @@ void enableRawMode() {
     atexit(disableRawMode);
 
     struct termios raw = E.orig_termios;
-    raw.c_iflag     &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    raw.c_oflag     &= ~(OPOST);
-    raw.c_cflag     |= (CS8);
-    raw.c_lflag     &= ~(ECHO | ICANON | IEXTEN | ISIG);
-    raw.c_cc[VMIN]  = 0;
-    raw.c_cc[VTIME] = 1;
+    raw.c_iflag        &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag        &= ~(OPOST);
+    raw.c_cflag        |= (CS8);
+    raw.c_lflag        &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN]     = 0;
+    raw.c_cc[VTIME]    = 1;
 
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }

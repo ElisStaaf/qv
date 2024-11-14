600bb4422a0d680ede45e20b902eb4a4098e209a
eliss <staaf.elis@gmail.com>
2024-11-14 17:43:53 +0100

    add __asm__ to C/C++ keywords

--- src/vine.c
+++ src/vine.c
@@ -171,16 +171,17 @@ struct editorTheme T = {
 char *C_HL_extensions[] = { ".c", ".h", ".cpp", ".hpp",
                             ".cc", ".hh", ".cxx", ".hxx", NULL };
 char *C_HL_keywords[] = {
-    "auto", "break", "case", "const", "continue", "default", "do", "else", "enum",  "extern", "for",
-    "goto", "if", "register", "return", "sizeof", "static",  "struct", "switch", "typedef",
-    "union", "volatile", "while", "NULL",  "alignas", "alignof", "and", "and_eq", "asm",
-    "bitand", "bitor", "class",  "compl", "constexpr", "const_cast", "deltype", "delete",
-    "dynamic_cast",  "explicit", "export", "false", "friend", "inline", "mutable",
-    "namespace",  "new", "noexcept", "not", "not_eq", "nullptr", "operator", "or",
-    "or_eq",  "private", "protected", "public", "reinterpret_cast", "static_assert",
-    "static_cast", "template", "this", "thread_local", "throw", "true", "try", "typeid",
-    "typename", "virtual", "xor", "xor_eq", "#define", "#include", "#if", "ifdef",
-    "#ifndef", "#endif", "#error", "#warning",
+    "auto", "break", "case", "const", "continue", "default", "do", "else", "enum",  "extern",
+    "for", "goto", "if", "register", "return", "sizeof", "static",  "struct", "switch", 
+    "typedef", "union", "volatile", "while", "__asm__", "NULL", "alignas", "alignof",
+    "and", "and_eq", "asm", "bitand", "bitor", "class",  "compl", "constexpr",
+    "const_cast", "deltype", "delete", "dynamic_cast",  "explicit", "export", "false",
+    "friend", "inline", "mutable", "namespace",  "new", "noexcept", "not", "not_eq",
+    "nullptr", "operator", "or", "or_eq",  "private", "protected", "public",
+    "reinterpret_cast", "static_assert", "static_cast", "template", "this",
+    "thread_local", "throw", "true", "try", "typeid", "typename", "virtual",
+    "xor", "xor_eq", "#define", "#include", "#if", "ifdef", "#ifndef",
+    "#endif", "#error", "#warning",
 
     "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
     "void|", "short|", "auto|", "bool|", NULL
